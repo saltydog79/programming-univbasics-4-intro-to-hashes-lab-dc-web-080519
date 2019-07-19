a5dcddb4ddb593c4788d66d0e2e2086c9c8d417d
Emily G <aspentree79@gmail.com>
2019-07-19 16:06:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,9 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    hash[key] += 1
+    key += 1
   else
-    hash[key] = 1
+    key = 1
   end
 
   # given a hash an a key as parameters, return an updated hash
